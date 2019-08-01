4af13d9d08b57e369b531b101ac35853db4c6daa
Bryan Dixon <javawolfpack@gmail.com>
2019-08-01 14:36:23 -0700

    Random create input + massive input test

--- createinput.py
+++ createinput.py
@@ -1,12 +1,15 @@
+from random import randrange
+
 try:
     n = int(input("N?"))
     f = open("input.txt", "w")
 
+    f.write(str(n)+ " 800 800\n")
     for i in range(n):
         for j in range(n):
-            f.write("0")
+            f.write(str(randrange(2)))
         f.write("\n")
     f.close()
-    
+
 except:
     print("Input needs to be an Integer")
