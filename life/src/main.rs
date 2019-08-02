4b873ed7ecda506813b87ad0fa53942c37c8b562
Bryan Dixon <javawolfpack@gmail.com>
2019-08-02 08:20:11 -0700

    Modified rust example to be MPI hello world

--- life/src/main.rs
+++ life/src/main.rs
@@ -9,39 +9,9 @@ fn main() {
     let size = world.size();
     let rank = world.rank();
 
-    let next_rank = if rank + 1 < size { rank + 1 } else { 0 };
-    let previous_rank = if rank > 0 { rank - 1 } else { size - 1 };
-
-    let msg = vec![rank, 2 * rank, 4 * rank];
-    mpi::request::scope(|scope| {
-        let _sreq = WaitGuard::from(
-            world
-                .process_at_rank(next_rank)
-                .immediate_send(scope, &msg[..]),
-        );
-
-        let (msg, status) = world.any_process().receive_vec();
-
-        println!(
-            "Process {} got message {:?}.\nStatus is: {:?}",
-            rank, msg, status
-        );
-        let x = status.source_rank();
-        assert_eq!(x, previous_rank);
-        assert_eq!(vec![x, 2 * x, 4 * x], msg);
-
-        let root_rank = 0;
-        let root_process = world.process_at_rank(root_rank);
-
-        let mut a;
-        if world.rank() == root_rank {
-            a = vec![2, 4, 8, 16];
-            println!("Root broadcasting value: {:?}.", &a[..]);
-        } else {
-            a = vec![0; 4];
-        }
-        root_process.broadcast_into(&mut a[..]);
-        println!("Rank {} received value: {:?}.", world.rank(), &a[..]);
-        assert_eq!(&a[..], &[2, 4, 8, 16]);
-    });
+    let processor = world.processor_name();
+    println!("Hello from task {} on {}!\n",rank,processor);
+    if rank==0{
+        println!("MASTER: Number of MPI tasks is: {}\n",size);
+    }
 }
