77c3305b2f5126a94e41fa54a1ef090ed59b4213
Bryan Dixon <javawolfpack@gmail.com>
2019-08-02 08:52:56 -0700

    Parsing first line of input file

--- life/src/main.rs
+++ life/src/main.rs
@@ -16,7 +16,7 @@ fn main() {
 
 
     if rank==0{
-        let args: Vec<String> = env::args().collect();
+        let mut args: Vec<String> = env::args().collect();
         println!("{:?}", args);
         if args.len() < 2{
             panic!("Requires at least 1 argument to run");
